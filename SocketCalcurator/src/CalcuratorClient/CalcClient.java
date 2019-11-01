7714cf04aebef3d0f82af28e1d294ae8c441c113
= <=>
2019-11-02 00:19:32 +0900

    SIMPLE CALC METHODS

--- SocketCalcurator/src/CalcuratorClient/CalcClient.java
+++ SocketCalcurator/src/CalcuratorClient/CalcClient.java
@@ -5,16 +5,25 @@ import java.net.*;
 import java.util.Scanner;
 
 public class CalcClient {
-    public static void main(String[] args) throws  Exception{
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String calcRequestLine = sc.nextLine()+"\n";
-        sc.close();
-        Socket connectToServer = new Socket("localhost",5180);
-        DataOutputStream outline = new DataOutputStream(connectToServer.getOutputStream());
-        outline.writeBytes(calcRequestLine);
-        BufferedReader readFromServer = new BufferedReader(new InputStreamReader(connectToServer.getInputStream()));
-        String result = readFromServer.readLine();
-        System.out.println(result);
-        connectToServer.close();
+        Socket connectToServer;
+        DataOutputStream outline;
+        BufferedReader readFromServer;
+        String calcRequestLine,result;
+        while (true) {
+
+            calcRequestLine= sc.nextLine() + "\n";
+            if(calcRequestLine=="-1\n")break;
+            connectToServer = new Socket("localhost", 5180);
+            outline = new DataOutputStream(connectToServer.getOutputStream());
+            outline.writeBytes(calcRequestLine);
+            readFromServer = new BufferedReader(new InputStreamReader(connectToServer.getInputStream()));
+            result = readFromServer.readLine();
+            connectToServer.close();
+            System.out.println(result);
+            System.out.println("Do you want more Calc?");
+            System.out.println("for exit input -1 or next line");
+        }
     }
 }
--- SocketCalcurator/src/CalcuratorServer/Calculator.java
+++ SocketCalcurator/src/CalcuratorServer/Calculator.java
@@ -8,44 +8,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class Calculator implements Runnable{
+public class Calculator implements Runnable {
     private String inputLine;
     private Socket socket;
     private String result;
 
-    Calculator(Socket socket)throws IOException {
+    Calculator(Socket socket) throws IOException {
         this.socket = socket;
         BufferedReader readBuff = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         inputLine = readBuff.readLine();
     }
-    private String calc(){
+
+    private String simpleCalc() {
+        int i, a, b;
+        boolean checkOperator = false;
+
+        for (i = 0; i < inputLine.length(); i++) {
+            if (inputLine.charAt(i) < '0' || inputLine.charAt(i) > '9') {
+                System.out.println(i+" "+inputLine.charAt(i));
+                checkOperator = true;
+                break;
+            }
+        }
+        if (checkOperator==false) {
+            return "There's no Operators";
+        }
+        a = Integer.parseInt(inputLine.substring(0, i));
+        b = Integer.parseInt(inputLine.substring(i+1));
+        switch (inputLine.charAt(i)) {
+            case 'x':
+                return "result : "+String.valueOf(a * b);
+            case '/':
+                if (b == 0)
+                    return "Error : Divide with 0";
+                return "result : "+String.valueOf((double) a / b);
+            case '+':
+                return "result : "+String.valueOf(a + b);
+            case '-':
+                return "result : "+ Integer.toString(a - b);
+                default: return "Error : No Supported Operator";
+        }
+    }
+
+    private String calc() {
+
         // TODO : divide With Operators
         // 1+2*3/(3-4)%4
-        int []position = new int[inputLine.length()];// ( 스택
-        for(int i=0;i<inputLine.length();i++){
+
+        int[] position = new int[inputLine.length()];// ( 스택
+        for (int i = 0; i < inputLine.length(); i++) {
 
         }
 
 
         return "0";
     }
-    private String SubCalc(String substr){
 
-    }
-    private void sendData()throws  IOException{
+    private void sendData() throws IOException {
         DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
-        outStream.writeBytes(result+'\n');
+        outStream.writeBytes(result + '\n');
         socket.close();
     }
+
     @Override
     public void run() {
         //The four basic arithmetic operations shoud be supported
         // - addition, subtraction, multiplication and division
-        result = String.valueOf(calc());
-        try{
+        result = simpleCalc();
+        try {
             sendData();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
